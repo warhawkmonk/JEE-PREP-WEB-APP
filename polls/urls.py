f20c4a2e265fd97ad6c7e2a2866a15b4b9343bbe
MAYANK RAJ <rajstarts35325@gmail.com>
2021-07-04 19:07:43 +0530

    commit to main

--- polls/urls.py
+++ polls/urls.py
@@ -20,6 +20,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]         
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)

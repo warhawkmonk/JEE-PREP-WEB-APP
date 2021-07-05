68519c7aa27b9508819ecb01c72a8f5278faa8fa
MAYANK RAJ <rajstarts35325@gmail.com>
2021-07-05 21:36:44 +0530

    to host

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,7 +101,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+ALLOWED_HOSTS = (
+    'your-app-name.herokuapp.com',
+    'localhost',
+)
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
